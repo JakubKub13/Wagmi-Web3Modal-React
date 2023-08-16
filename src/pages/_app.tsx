eb8f5482626e64f1a85ea867a20702a511c01b9f
JakubKub13 <jakubkubala3@gmail.com>
2023-08-16 09:33:20 +0200

    init commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,37 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/react'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { goerli } from 'wagmi/chains'
+
+
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const chains = [goerli]
+  const projectId = "5a9720ca34061a69e9315b521542b273"
+
+  const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors: w3mConnectors({ projectId, chains }),
+    publicClient
+  })
+  const ethereumClient = new EthereumClient(wagmiConfig, chains)
+
+  return (
+  <>
+    <WagmiConfig config={wagmiConfig}>
+     <Component {...pageProps} />
+    </WagmiConfig>
+
+    <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+  </>
+  )
+
+  
 }
+
+
